be2dbac44fd69a2a71a9b3f8d94247c4b182124d
Andile Dube <andiledube41@gmail.com>
2022-04-27 20:52:48 +0200

    quiz

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText = 'time';
+  bool questionAnswer = false;
+
+  Question(this.questionText, this.questionAnswer) {
+    // questionText = q;
+    // questionAnswer = a;
+  }
+}
